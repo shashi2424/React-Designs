6ecba594db605f7f4e230bae1e251078c0a1724d
Shashi_2424 <shashikumar@way2news.com>
2023-05-26 09:40:39 +0530

    second page added

--- Web Developer Portfolio/portfolio/src/components/Card2/SecondCard.js
+++ Web Developer Portfolio/portfolio/src/components/Card2/SecondCard.js
@@ -3,7 +3,27 @@ import './second.css'
 function SecondCard() {
     return (
         <div className="Card2">
-            lkjnjknfgjknkjfsg
+            <div className="lcard2">
+                <p className="spense">Spense.com</p>
+                <span className="arrow">&#8594;</span>
+                <p className="rethinking">Rethinking the way writes get paid, an open-source platform designed to help writers focus more on writing, and less on when and how they'll get paid. Project in collaboration with Codewell.cc</p>
+            </div>
+            <div className="rcard2">
+                <p className="yelp">YelpCamp.com</p>
+                <span className="arrow">&#8594;</span>
+                <p className='allowing'>Allowing backpack travelers to perfectly plan their trip through an open-source platform similar to TripAdvisor. With over 1m MAU, YelpCamp has been the crowd's favorite in 2021.</p>
+            </div>
+
+            <div className="grayback">
+                <div className="lgray">
+                    <p className='cofound'>A co-founder at one of the world's largest communities.</p>
+                    <p className="combined">The combined experience i have working at the top Fortune 500 comanies has allowed me to developer a skillset that helps me in not just development, but in every aspect of any business <br /><br /> I'm proud to announce that i am working at one og the world's larges tcommunities teaching young minds about how to self yourself as a developer and open them to a whole new world of opportunities. </p>
+                </div>
+                <div className='rgray'>
+                    <p>As a developer, you have everything available to you and all that's holding you back is yourself. <br /><br /> A quote i live by perfectly illustrates what i mean. <br /><br /> "How big would you dream,if you <b> knew</b> you wouldn't fail?" You're already gone through the hardest parts being a developer,it's time to elevate your skills and become a leader in something you're passionate about. <br /><br />I'm happy to chat over coffee some time about how can help your company.</p>
+                </div>
+            </div>
+
         </div>
     )
 }
